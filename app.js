967e6dddc371dc715dfed6a53a84e3ce1fcebb49
Matheus Lucas <matheuslucas562@yahoo.com.br>
2023-07-12 21:52:35 -0300

    navegando-no-app

--- app.js
+++ app.js
@@ -6,8 +6,10 @@ const rootRouter = require("./src/routes/index");
 require("./config/database");
 
 const app = express();
-
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "public")));
+
 app.set("views", path.join(__dirname, "src/views"));
 app.set("view engine", "ejs");
 
