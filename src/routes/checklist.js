29a0b3c7200e5c963777321a535c947a0993918c
Matheus Lucas <matheuslucas562@yahoo.com.br>
2023-07-18 02:09:52 -0300

    atualizacao-dados-put

--- src/routes/checklist.js
+++ src/routes/checklist.js
@@ -24,7 +24,18 @@ router.get("/new", async (req, res) => {
   } catch (error) {
     res
       .status(500)
-      .render("pages/error", { errors: "Erro ao carregar o formulário" });
+      .render("pages/error", { error: "Erro ao carregar o formulário" });
+  }
+});
+
+router.get("/:id/edit", async (req, res) => {
+  try {
+    let checklist = await Checklist.findById(req.params.id);
+    res.status(200).render("checklists/edit", { checklist: checklist });
+  } catch (error) {
+    res.status(500).render("pages/error", {
+      error: "Erro ao exibir a edição da Listas de tarefas",
+    });
   }
 });
 
@@ -55,12 +66,16 @@ router.get("/:id", async (req, res) => {
 
 router.put("/:id", async (req, res) => {
   let { name } = req.body.checklist;
+  let checklist = await Checklist.findById(req.params.id);
 
   try {
-    let checklist = await Checklist.findByIdAndUpdate(req.params.id, {name}, {new: true})
-    res.status(200).json(checklist)
+    await checklist.updateOne({ name });
+    res.redirect("/checklists");
   } catch (error) {
-    res.status(422).json(error);
+    let errors = error.errors;
+    res
+      .status(422)
+      .render("checklists/edit", { checklist: { ...checklist, errors } });
   }
 });
 
